1d63d810ad22c7ae4d85bc9f21b850fc69f2b552
Art Frich <frich.g23@yandex.ru>
2023-12-21 15:59:32 +0500

    fix: fix gh-pages

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import { resolve } from 'path';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/Todo-for-strong-team/',
   server: {
     host: true,
     port: 5173,
